6ca501f8a9a8efdbf752d8c8bbfb8e494c9bbe54
tom.maxwellmans@gmail.com <tom.maxwellmans@gmail.com>
2020-02-16 13:03:29 +1300

    Updated splash page

--- src/states/intro.js
+++ src/states/intro.js
@@ -2,16 +2,16 @@ var intro = new Kiwi.State('intro');
 
 intro.preload = function () {
     Kiwi.State.prototype.preload.call(this);
-    this.addImage('spritesheet1', 'spritesheet1.png');
+    this.addImage('splashArt', 'asset/splashArt.png');
 };
 
 intro.create = function () {
 
     Kiwi.State.prototype.create.call(this);
 
-    this.spritesheet1 = new Kiwi.GameObjects.StaticImage(this, this.textures.spritesheet1, 0, 0);
+    this.splashArt = new Kiwi.GameObjects.StaticImage(this, this.textures.splashArt, 0, 0);
 
-    this.addChild(this.spritesheet1);
+    this.addChild(this.splashArt);
 
 };
 
