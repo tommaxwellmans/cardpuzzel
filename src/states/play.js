c0e5d59fa5bcb0481cfac40333511438220a4e0a
tom.maxwellmans@gmail.com <tom.maxwellmans@gmail.com>
2020-02-16 13:21:44 +1300

    Added cards to the play state

--- src/states/play.js
+++ src/states/play.js
@@ -5,7 +5,9 @@ play.preload = function () {
     Kiwi.State.prototype.preload.call(this);
     this.addImage('background', 'background.png');
 
-    this.addImage('redAndBlueAttackCard', 'asset/card/redAndBlueAttack.png')
+    this.addImage('redAndBlueAttackCard', 'asset/card/redAndBlueAttack.png');
+    this.addImage('yellowAndBlueAttack', 'asset/card/yellowAndBlueAttack.png');
+    this.addImage('yellowAndRedAttack', 'asset/card/yellowAndRedAttack.png');
 
 };
 
@@ -16,11 +18,14 @@ play.create = function () {
     this.background = new Kiwi.GameObjects.StaticImage(this, this.textures.background, 0, 0);
 
     this.redAndBlueAttachCardImage = new Kiwi.GameObjects.StaticImage(this, this.textures.redAndBlueAttackCard, 40, 500);
+    this.yellowAndBlueAttack = new Kiwi.GameObjects.StaticImage(this, this.textures.yellowAndBlueAttack, 240, 500);
+    this.yellowAndRedAttack = new Kiwi.GameObjects.StaticImage(this, this.textures.yellowAndRedAttack, 440, 500);
 
     // create hand object to render in the correct place
     this.hand = new Kiwi.Group(this);
     this.hand.addChild(this.redAndBlueAttachCardImage);
-
+    this.hand.addChild(this.yellowAndBlueAttack);
+    this.hand.addChild(this.yellowAndRedAttack);
 
     this.addChild(this.background);
 
