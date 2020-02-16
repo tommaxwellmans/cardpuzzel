fb8227dc35d1bc83621fb57f07dec8ba12cc27e8
tom.maxwellmans@gmail.com <tom.maxwellmans@gmail.com>
2020-02-16 13:15:19 +1300

    Added cards to the play stage

--- src/game.js
+++ src/game.js
@@ -1,1 +1,7 @@
-var game = new Kiwi.Game();
+var gameOptions = {
+    renderer: Kiwi.RENDERER_WEBGL,
+    width: 800,
+    height: 800
+};
+
+var game = new Kiwi.Game('content', 'myGame', null, gameOptions);
--- src/states/play.js
+++ src/states/play.js
@@ -4,6 +4,9 @@ var play = new Kiwi.State('play');
 play.preload = function () {
     Kiwi.State.prototype.preload.call(this);
     this.addImage('background', 'background.png');
+
+    this.addImage('redAndBlueAttackCard', 'asset/card/redAndBlueAttack.png')
+
 };
 
 play.create = function () {
@@ -12,7 +15,16 @@ play.create = function () {
 
     this.background = new Kiwi.GameObjects.StaticImage(this, this.textures.background, 0, 0);
 
+    this.redAndBlueAttachCardImage = new Kiwi.GameObjects.StaticImage(this, this.textures.redAndBlueAttackCard, 40, 500);
+
+    // create hand object to render in the correct place
+    this.hand = new Kiwi.Group(this);
+    this.hand.addChild(this.redAndBlueAttachCardImage);
+
+
     this.addChild(this.background);
+
+    this.addChild(this.hand);
 
 };
 
