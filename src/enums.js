ee45898f057aae0c0d31d29394c207488405a937
tom.maxwellmans@gmail.com <tom.maxwellmans@gmail.com>
2020-02-17 13:11:26 +1300

    made screen larger

--- src/enums.js
+++ src/enums.js
@@ -4,4 +4,4 @@ const CatColor = Object.freeze( { 'White': 0,  'Black': 1} );
 
 const LaneNumber = Object.freeze( { 'Lane1': 0,  'Lane2': 1, 'Lane3': 2} );
 
-const GameDimension = Object.freeze( { 'Width': 800, 'Height': 800, } );
+const GameDimension = Object.freeze( { 'Width': 1400, 'Height': 900, } );
